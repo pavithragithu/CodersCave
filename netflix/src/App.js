2e6d72511f6c824c39f19c522d4d17f4c7d2fe6b
pavithragithu <jaraplapavithra248@gmail.com>
2023-09-22 02:17:39 -0700

    first commit

---
+++ netflix/src/App.js
@@ -0,0 +1,24 @@
+import Home from './Home.js'
+import './App.css';
+import Tv from './Tv.js';
+import Mobile from './mobile.js';
+import Tv2 from './tv2.js';
+import Child from './child.js';
+import Query from './query.js';
+import Footer from './footer.js';
+
+function App() {
+  return (
+   <>
+    <Home/>
+     <Tv/>
+     <Mobile/>
+     <Tv2/>
+     <Child/>
+     <Query/>
+     <Footer/>
+     </>
+  );
+}
+
+export default App;
